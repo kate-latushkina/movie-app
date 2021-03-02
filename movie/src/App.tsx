e38b3c10872e50f9c6956a75d3625c8749636f07
Katsiaryna Latushkina <klatushkina@colemanrg.com>
2021-03-02 18:41:13 +0300

    fix rename folder

--- movie/src/App.tsx
+++ movie/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "./components/Footer";
-import Header from "./components/header";
+import Header from "./components/Header";
 
 export const App: React.FC = () => {
   return (
