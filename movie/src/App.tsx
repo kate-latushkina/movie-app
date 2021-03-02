3d36d35b8c3b478a508a9427027bcafd1ec52e8c
Katsiaryna Latushkina <klatushkina@colemanrg.com>
2021-03-02 19:08:48 +0300

    fix: renamed directory

--- movie/src/App.tsx
+++ movie/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "./components/Footer";
-import Main from './components/main';
+import Main from './components/Main';
 import Header from "./components/Header";
 
 export const App: React.FC = () => {
