bdbba270c7ac0e26e2f43bcc48c326a43f1915e2
Katsiaryna Latushkina <katsiaryna_latushkina@epam.com>
2021-04-23 14:30:08 +0300

    feat(test): finish l6

--- movie/src/components/Main/Categories/__test__/Categories.spec.tsx
+++ movie/src/components/Main/Categories/__test__/Categories.spec.tsx
@@ -1,23 +1,35 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import renderer from "react-test-renderer";
 import { Tabs } from "@material-ui/core";
 import Categories from "../Categories";
 
 describe("should render Categories component", () => {
   let component: any;
+  let tabs: any;
 
   beforeEach(() => {
-    component = shallow(<Categories />);
+    component = mount(<Categories />);
+    tabs = component.find(Tabs);
   });
 
   it("should contain Tabs", () => {
-    const wrapper = component.find(Tabs);
-    expect(wrapper.length).toBe(1);
+    expect(tabs.length).toBe(1);
   });
 
   it("should render Categories snapshot", () => {
     const wrapper = renderer.create(<Categories />).toJSON();
     expect(wrapper).toMatchSnapshot();
+  });
+  describe("should render Tabs state", () => {
+    it("Tabs loads with initial state of 0", () => {
+      expect(tabs.props().value).toEqual(0);
+    });
+
+    it("should handler Tabs set new value", () => {
+      tabs.simulate("change");
+      tabs.props().value = 1;
+      expect(tabs.props().value).toBe(1);
+    });
   });
 });
