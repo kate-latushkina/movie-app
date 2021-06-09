89592716426e758aeedeebf48b75689cc65235ca
Katsiaryna Latushkina <katsiaryna_latushkina@epam.com>
2021-06-09 13:48:11 +0300

    feat(search-movie): add request to search movie

--- movie/src/state/reducers/actionCreators.ts
+++ movie/src/state/reducers/actionCreators.ts
@@ -1,7 +1,8 @@
 import { Dispatch } from "redux";
 import { AllMoviesState } from "./allMoviesReducer";
 import { MovieState } from "./movieReducer";
-import { addMovies } from "../../variables/Api";
+import { addMovies, API_KEY } from "../../variables/Api";
+import { Request } from "../../variables/Request";
 
 const SET_MOVIE = "SET_MOVIE";
 const SET_ALL_MOVIES = "SET_ALL_MOVIES";
@@ -22,5 +23,14 @@ export function setAllMovies(movies: AllMoviesState[]) {
 
 export const getAllMovies = () => async (dispatch: Dispatch) => {
   const result = await addMovies.then(res => res);
+  dispatch(setAllMovies(result));
+};
+
+export const getSearchMovies = (searchValue: string) => async (
+  dispatch: Dispatch,
+) => {
+  const GET_SEARTCH = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${searchValue}&page=1&include_adult=false`;
+
+  const result = await Request.getRequest(GET_SEARTCH).then(res => res);
   dispatch(setAllMovies(result));
 };
